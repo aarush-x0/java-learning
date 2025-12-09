e622d4f4b8ea9100cd88ee2a4084e0601b1bd2ce
aarush-x0 <aarushxsingh07@gmail.com>
2025-12-09 23:08:08 +0530

    bug fixed

--- loops/patterns/Rectangle_Pattern.java
+++ loops/patterns/Rectangle_Pattern.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Rectangle_Pattern {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter rows: ");
+            int rows = sc.nextInt();
 
-        System.out.print("Enter rows: ");
-        int rows = sc.nextInt();
+            System.out.print("Enter columns: ");
+            int cols = sc.nextInt();
 
-        System.out.print("Enter columns: ");
-        int cols = sc.nextInt();
-
-        for(int i = 1; i <= rows; i++){
-            for(int j = 1; j <= cols; j++){
-                System.out.print("* ");
+            for(int i = 1; i <= rows; i++){
+                for(int j = 1; j <= cols; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
